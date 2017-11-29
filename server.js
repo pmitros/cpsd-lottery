fb0e0049221248deb5d7cc1069c5943b958ade6d
Piotr Mitros <piotr@mitros.org>
2017-11-29 11:20:18 -0500

    Index and lottery

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ function printTabs(out, which){
 }
 
 app.get('/', function(req, res){
-    fs.readFile("public/becca.html", 'utf8', function (err,data) {
+    fs.readFile("public/index.html", 'utf8', function (err,data) {
       if (err) {
         return console.log(err);
       }
